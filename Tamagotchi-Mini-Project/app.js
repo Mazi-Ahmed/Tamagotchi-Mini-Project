7fc1ec9c3ac088ac339e069fde4ace97556d4f8c
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-04-24 20:09:09 -0400

    Foundation Created and Linked Pages

---
+++ Tamagotchi-Mini-Project/app.js
@@ -0,0 +1,15 @@
+// Create a Game class
+class Game {
+    constructor(name){
+        this.name = name
+        this.hunger = 1
+        this.sleepiness = 1
+        this.boredom = 1
+        this.age = 0
+    }
+// increase your pet's age     
+    upAge () {
+        this.age++
+    }
+    
+}
