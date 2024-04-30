a87db36a0f6471a2bbae8869fa0ed0615e02b46c
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-04-30 13:06:17 -0400

    Final Commit

--- Tamagotchi-Mini-Project/app.js
+++ Tamagotchi-Mini-Project/app.js
@@ -1,3 +1,5 @@
+const message = document.getElementById('messages')
+
 // Create a class
 class Tamagotchi {
     constructor(name){
@@ -25,14 +27,9 @@ class Tamagotchi {
         this.hunger++
         this.sleepiness++
         this.boredom++
-        
-        if(this.hunger === 10 || this.sleepiness === 10 || this.boredom === 10) {
-            console.log(`${this.name} has fainted!`);
-            clearInterval(this.interval);
         }
     }
-}
-// Domcontent for Metrics
+// Variables
 let tamagotchi;
 
 const pokeImg = document.getElementById('egg');
@@ -41,35 +38,21 @@ const hungerSpan = document.getElementById('hunger');
 const sleepinessSpan = document.getElementById('sleepiness');
 const boredomSpan = document.getElementById('boredom');
 const ageSpan = document.getElementById('age');
+const feedButton = document.getElementById('feed');
+const lightsButton = document.getElementById('sleep');
+const playButton = document.getElementById('play');
 
+const disableControls = () => {
+    feedButton.disabled = true;
+    lightsButton.disabled = true;
+    playButton.disabled = true;
+  };
 
-// Set Name Function 
- document.getElementById('setName').addEventListener('click', () => {
-    const putName = document.getElementById('putName');
-    tamagotchi = new Tamagotchi(putName.value);
-    nameSpan.textContent = tamagotchi.name
- })
-// Button Functions for Metrics
- document.getElementById('feed').addEventListener('click', () => {
-    if(tamagotchi) {
-        tamagotchi.feed();
-        hungerSpan.textContent = tamagotchi.hunger;
-    }
- })
- 
- document.getElementById('sleep').addEventListener('click', () => {
-    if(tamagotchi) {
-        tamagotchi.sleep();
-        sleepinessSpan.textContent = tamagotchi.sleepiness
-    }
- })
- 
-document.getElementById('play').addEventListener('click', () => {
-    if(tamagotchi) {
-        tamagotchi.play(); 
-        boredomSpan.textContent = tamagotchi.boredom;
-    }
-})
+  const enableControls = () => {
+    feedButton.disabled = false;
+    lightsButton.disabled = false;
+    playButton.disabled = false;
+  };
 
 const updateMetrics = () => {
     if (tamagotchi) {
@@ -77,24 +60,67 @@ const updateMetrics = () => {
         hungerSpan.textContent = tamagotchi.hunger;
         sleepinessSpan.textContent = tamagotchi.sleepiness;
         boredomSpan.textContent = tamagotchi.boredom;
-    if (tamagotchi.age === 5) {
-        pokeImg.src = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full//004.png'
+    
+    if(tamagotchi.hunger === 10 || tamagotchi.sleepiness === 10 || tamagotchi.boredom === 10) {
+            message.textContent = (`${tamagotchi.name} has fainted!`);
+            clearInterval(tamagotchi.interval);
+            disableControls();
     }
-    if (tamagotchi.age === 16) {
-        pokeImg.src = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full//005.png'
+    if (tamagotchi.age === 1) {
+        pokeImg.src = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e7948d7f-37fa-43f3-b3ad-69390c3ec4ce/d6b3l7g-c8aa2c72-3233-4e86-bb2f-c273cf58e739.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2U3OTQ4ZDdmLTM3ZmEtNDNmMy1iM2FkLTY5MzkwYzNlYzRjZVwvZDZiM2w3Zy1jOGFhMmM3Mi0zMjMzLTRlODYtYmIyZi1jMjczY2Y1OGU3MzkuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.69YO8IOy6hEVC9DvtbXQUhblAxIOAqFsAT4goYlZX_k'
+        alert('Oh?')
     }
-    if (tamagotchi.age === 36) {
-        pokeImg.src = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/006.png'
+    if (tamagotchi.age === 8) {
+        pokeImg.src = 'https://64.media.tumblr.com/a00892f48f92f5006d9f55e99b17253d/tumblr_nozmvuXqCf1scncwdo1_540.gif'
+        alert(`What? ${tamagotchi.name} is evolving!`)
+    }
+    if (tamagotchi.age === 12) {
+        pokeImg.src = 'https://31.media.tumblr.com/d35d35b0e9ad02705290fa10ba995556/tumblr_ms7wd8K2tL1scncwdo1_500.gif'
+        alert(`What? ${tamagotchi.name} is evolving!`)
     }
     ageSpan.textContent = tamagotchi.age;
 }
 }
 
+// Set Name Function 
+document.getElementById('setName').addEventListener('click', () => {
+    const putName = document.getElementById('putName');
+    if (putName.value.trim() !== '') {
+        tamagotchi = new Tamagotchi(putName.value.trim());
+        nameSpan.textContent = tamagotchi.name;
+        enableControls();
+        tamagotchi.interval = setInterval(() => {
+            tamagotchi.upAge();
+            updateMetrics();
+        }, 5000);
+    } else {
+        alert('Please enter a Nickname.')
+    }
+ })
+
+// Button Functions for Metrics
+ feedButton.addEventListener('click', () => {
+    if(tamagotchi) {
+        tamagotchi.feed();
+        hungerSpan.textContent = tamagotchi.hunger;
+    }
+ })
+ 
+lightsButton.addEventListener('click', () => {
+    if(tamagotchi) {
+        tamagotchi.sleep();
+        sleepinessSpan.textContent = tamagotchi.sleepiness
+    }
+ })
+ 
+playButton.addEventListener('click', () => {
+    if(tamagotchi) {
+        tamagotchi.play(); 
+        boredomSpan.textContent = tamagotchi.boredom;
+    }
+})
+
 tamagotchi = new Tamagotchi('Poke')
 nameSpan.textContent = tamagotchi.name; 
 
-tamagotchi.interval = setInterval(() => {
-    tamagotchi.upAge();
-    updateMetrics();
-}, 3000);
 
